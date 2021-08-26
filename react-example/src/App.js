397f7a0c561a585e9732c70dcac874cdd94752d6
Agustin <agualvila@gmail.com>
2021-08-25 21:16:31 -0300

    comments

--- react-example/src/App.js
+++ react-example/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 
+// Funcion que devuelve nombre + apellido a partir de un objeto
 function formatName(user) {
   if (user) {
     return user.name + " " + user.lastname;
   }
 }
 
+// Funcion que retorna un <p> con saludo usando formatName
 function saludo(user) {
   if (user) {
     return <p>Hola, {formatName(user)}!</p>;
@@ -13,28 +15,32 @@ function saludo(user) {
   return <p> Hola, sujeto desconocido</p>
 }
 
+// Ejemplo de usuario 1
 const persona_1 = {
   name: "Pepito",
   lastname: "Lopez"
 }
 
+// Ejemplo de usuario 2
 const persona_2 = {
   name: "Fulano",
   lastname: "Perez"
 }
 
+// Variable que contiene un unico elemento <p>
 const elementSimple = <p>Este parrafo esta en una variable</p>;
 
+// variable que contiene varios elementos dentro de un unico elemento <div>
 const elementComplex = (
   <div>
     <h2>Elemento compuesto</h2>
     <p>Este elemento esta conformado por varias etiquetas dentro de un div</p>
     <p>Otro parrafo</p>
     <p>Oooootro parrafo</p>
-
   </div>
 )
 
+// APP
 function App() {
   return (
     <div className="App">
